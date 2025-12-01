6e0644656b9890eadbc6c3d5528dfa579e125209
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-12-01 14:59:17 +0000

    Revert "core: Introduce PropsHooksUtils"
    This reverts commit af6513017a486535521e604b451bbba576d104fb.

--- core/java/android/app/Application.java
+++ core/java/android/app/Application.java
@@ -345,7 +345,6 @@ public class Application extends ContextWrapper implements ComponentCallbacks2 {
     /* package */ final void attach(Context context) {
         attachBaseContext(context);
         mLoadedApk = ContextImpl.getImpl(context).mPackageInfo;
-        com.android.internal.util.android.PropsHooksUtils.setProps(context);
     }
 
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.R, trackingBug = 170729553)
